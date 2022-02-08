bf3692e06a37dd304fce72361d4eadec6d11c1ff
Rock <59310502@go.buu.ac.th>
2022-02-08 13:10:27 +0700

    event01

--- resources/views/event.blade.php
+++ resources/views/event.blade.php
@@ -37,28 +37,15 @@
         <!-- Page Content-->
         <div class="container-fluid p-0">
             <!-- About-->
-            <section class="resume-section" id="education">
+            <section class="resume-section" id="about">
                 <div class="resume-section-content">
-                    <h2 class="mb-5">Education</h2>
-                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
-                        <div class="flex-grow-1">
-                            <h3 class="mb-0">Burapha University</h3>
-                            <div class="subheading mb-3">Science and Arts </div>
-                            <div>Information Technology </div>
-                            <p>GPA: 2.21 </p>
-                        </div>
-                        <div class="flex-shrink-0"><span class="text-primary">August 2016 - December 2020</span></div>
-                    </div>
-                    <div class="d-flex flex-column flex-md-row justify-content-between">
-                        <div class="flex-grow-1">
-                            <h3 class="mb-0">Trattakankun School</h3>
-                            <div class="subheading mb-3"> Mathematics-Science Programme.</div>
-                            <p>GPA: 3.56</p>
-                        </div>
-                        <div class="flex-shrink-0"><span class="text-primary"> Feb 2014 </span></div>
-                    </div>
-                </div>
+                    <h1 class="mb-0">
+                        SARAN
+                        <span class="text-primary">POKSOMBAT</span>
+                    
             </section>
+            <hr class="m-0" />
+        </div>   
         <!-- Bootstrap core JS-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
         <!-- Core theme JS-->
--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,6 @@ Route::get('/experience', function () {
 Route::get('/skill', function () {
     return view('skill');
 });
+Route::get('/event', function () {
+    return view('event');
+});
