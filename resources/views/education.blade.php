5d4ebd0140811e6b5c24537ef45e1ff9d2d7340b
Rock <59310502@go.buu.ac.th>
2022-02-08 12:15:15 +0700

    edu

---
+++ resources/views/education.blade.php
@@ -0,0 +1,27 @@
+@extends('tempate\layout')
+
+@section('deucation')
+<section class="resume-section" id="education">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Education</h2>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Burapha University</h3>
+                            <div class="subheading mb-3">Science and Arts </div>
+                            <div>Information Technology </div>
+                            <p>GPA: 2.21 </p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">August 2016 - December 2020</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Trattakankun School</h3>
+                            <div class="subheading mb-3"> Mathematics-Science Programme.</div>
+                            <p>GPA: 3.56</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary"> Feb 2014 </span></div>
+                    </div>
+                </div>
+            </section>
+@section('content')
+@endsection
